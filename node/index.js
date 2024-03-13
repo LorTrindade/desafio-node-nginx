720e9561f69c5c571615dc0bffe9443dd635dbf5
Lorhana Trindade <lortrindade@gmail.com>
2024-03-12 21:28:48 -0300

    fix: fixing full cycle challenge

--- node/index.js
+++ node/index.js
@@ -3,17 +3,25 @@ const app = express()
 const port = 3000
 const mysql = require('mysql')
 
+const connection = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb',
+})
+const sql = `INSERT INTO people(name) values('Marta')`
+connection.query(sql)
+
 app.get('/', (req, res) => {
-    const connection = mysql.createConnection({
-        host: 'db',
-        user: 'root',
-        password: 'root',
-        database: 'nodepeople',
+    const query = `SELECT name FROM people`
+    const people = connection.query(query, function (err, result, fields) {
+        let table = '<table>'
+        for (const person of result) {
+            table += `<tr><td>${person.name}</td></tr>`
+        }
+        table += '</table>'
+        res.send('<h1>Full Cycle Rocks! </h1>' + table)
     })
-    
-    const sql = `INSERT INTO peoplee(name) values('Marta')`
-    connection.query(sql)
-    res.send('<h1>Full Cycle Rocks!</h1>')
 })
 
 app.listen(port, () => {
